8e07d7f4d93eb12b8d4e9315888091f31fe2c8c9
chenthehero <ericchen67@gmail.com>
2020-02-26 15:59:42 -0500

    Added projects scaffold with foreign key to customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_202215) do
+ActiveRecord::Schema.define(version: 2020_02_26_203640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,4 +25,13 @@ ActiveRecord::Schema.define(version: 2020_02_26_202215) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "project_name"
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_projects_on_customer_id"
+  end
+
+  add_foreign_key "projects", "customers"
 end
